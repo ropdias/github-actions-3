5f516ba4285c08aba1495b22ad8d0c53c0cd2de6
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2023-01-08 23:37:04 -0300

    another comment

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App';
 import './index.css';
 
 // This is a comment, testing skip workflow
+// This is another comment
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
